a4bc1571fa611a28ec13ef45a3f0fc4bf07b1d71
Junghwan Rhee <jhrhee@gmail.com>
2026-09-20 11:15:38 -0400

    adding llvm 21 packages

--- config.sh
+++ config.sh
@@ -6,6 +6,12 @@ sudo apt intall -y python3 python3-pip make git wget rsync
 
 sudo apt install -y \
   build-essential autoconf automake autopoint \
-  bison gettext gperf texinfo git rsync xz-utils
+  bison gettext gperf texinfo git rsync xz-utils \
+  lsb-release wget curl software-properties-common gnupg
+
+sudo apt install -y \
+  llvm-21-dev llvm-21-tools \
+  clang-21 clang-tools-21 \
+  libclang-21-dev lld-21
 
 ./create_coreutils.sh
